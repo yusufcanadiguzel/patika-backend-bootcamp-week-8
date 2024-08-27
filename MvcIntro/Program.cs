f355e5f4f51275d4811eeb0a669bfe29473abac4
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-08-27 15:00:15 +0300

    Add project files.

---
+++ MvcIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+//Controller: Client tarafından gelen isteklerin yönetildiği katmandır. Model ve View arasındaki akışı kontrol eder.
+
+//Action: Controller içerisinde tanımlanan clientten gelen istekleri karşılamak için kullanılan metotlardır.
+
+//Model: Tüm veri işlemlerinin gerçekleştiği katmandır. Veritabanından veri çekmekten, iş kurallarının uygulanmasına kadar tüm işlemler burada gerçekleşir.
+
+//View: Uygulamanın son kullanıcıyla etkileşime girdiği katmandır. UI ile ilgili herşey bu katmanda bulunur.
+
+//Razor: C# kodları ile HTML kodlarını bir arada kullanrak web uygulaması geliştirilmesine olanak sağlayan teknolojidir.
+
+//RazorView: C# ile HTML kodlarının bir arada kullanılabilmesini sağlayan teknoloji bileşenidir.
+
+//wwwroot: Uygulamanın içerisinde kullanılacak statik elemanları barındırır. Bunlara CSS, JS ve images örnek gösterilebilir.
---
+++ MvcIntro/README.md
@@ -0,0 +1,22 @@
+# Mvc Intro Project
+
+This is the mvc intro project for week 8 of the Patika+ Back-End Program. 
+
+Technologies used in the project. They are:
+- C#
+- ASP.NET Core
+
+### Purpose
+- The purpose is to go over what was covered in lessons.
+
+### Requirements
+
+1. Create an ASP.NET Core Empty project.
+2. Create a folder named Controllers.
+3. Create a folder named Models.
+4. Create a folder named Views.
+5. Create a folder named wwwroot.
+6. Make configurations for use static files in wwwroot.
+7. Make routing configurations.
+8. Make a default routing configuration.
+9. Explanation this terms in program.cs: Controller, Action, Model, View, Razor, RazorView, wwwroot
